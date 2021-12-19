be7fcd372c5de00d1fed26a1f0ccd9bb1c24f0b2
kulataev_kanat <KulataevKanat01@gmail.com>
2021-12-19 09:23:07 +0300

    fixed

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
 
 from api.models import User, Object
 
 admin.site.register(User)
-admin.site.register(Group)
 admin.site.register(Object)
